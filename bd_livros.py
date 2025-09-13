acc9947a39a444be7afcbdf19cab333974fbc031
Beatriz Silva <beatrizsilva20875@gmail.com>
2025-09-13 10:31:50 -0300

    adicionado api criado com flask

---
+++ bd_livros.py
@@ -0,0 +1,9 @@
+# Criamos um ambiente virtual com o comando
+
+livros = [
+    {'id':1,'titulo':'o pequeno principe','autor':'Antoine de Saint-Exupéry','ano':1943},
+    {'id':2,'titulo':'1984','autor':'George Orwell','ano':1949},
+    {'id':3,'titulo':'Dom Casmurro','autor':'Machado de Assis','ano':1899},
+    {'id':4,'titulo':'Clean Code','autor':'Robert C. Martim','ano':2008},
+    {'id':5,'titulo':'A Revolução dos Bichos','autor':'George Orwell','ano':1945}
+]
